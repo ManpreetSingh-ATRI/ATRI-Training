567ed49a480076a1a530820e4dbf461efa0715f7
ManpreetSingh-ATRI <msingh60@atrihub.io>
2022-01-25 22:14:03 -0800

    feat(TDD Training) Chapter 4
    The task in this chapter was to follow TDD to move our index view from hard-coded html to a template based view. Fill up values on the template and run tests to check the view.
    
    ICTRAINING-951

--- TDD-training/functional_tests.py
+++ TDD-training/functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
@@ -10,8 +12,34 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get('http://localhost:8000')
+        self.browser.get('http://localhost:8000/lists')
         self.assertIn('To-Do', self.browser.title)
+
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # Invited to enter a to-do item straight away
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            input_box.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # Types "Buy peacock feathers" into a text box
+        input_box.send_keys('Buy peacock feathers')
+
+        # On hitting enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list table
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
+        )
+
         self.fail('Finish the test!')
 
 
--- TDD-training/lists/tests.py
+++ TDD-training/lists/tests.py
@@ -1,19 +1,11 @@
 from django.test import TestCase
 from django.urls import reverse
-from django.http import HttpRequest
-from lists import views
 
 
 class HomePageTest(TestCase):
-    def test_root_url_resolves_to_index_view(self):
+
+    def test_uses_index_template(self):
         url = reverse('lists:index')
         response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
 
-    def test_root_url_returns_correct_html(self):
-        request = HttpRequest()
-        response = views.index(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        self.assertTemplateUsed(response, 'lists/index.html')
